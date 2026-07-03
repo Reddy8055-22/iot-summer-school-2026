600a62577b6876118a706eae6679733cc968a318
Reddy8055-22 <sushanthreddypalle22@gmail.com>
2026-07-03 09:51:21 +0000

    Add week 1 LED blink sketch

---
+++ iot-summer-school-2026/week1/led_blink/led_blink.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // Initialize digital pin 13 as an output.
+  pinMode(13, OUTPUT );
+
+}
+
+void loop() {
+  digitalWrite( 13 ,HIGH );   // Turn the LED on
+  delay(1000);              // Wait for a second (1000 milliseconds)
+  digitalWrite(13, LOW) ;    // Turn the LED off
+  delay(1000);              // Wait for a second
+}
