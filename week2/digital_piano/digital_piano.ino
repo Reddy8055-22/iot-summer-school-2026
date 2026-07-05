899cf75ebb9f60b00f59073b2059353ce6df9e7f
Reddy8055-22 <sushanthreddypalle22@gmail.com>
2026-07-05 15:19:13 +0000

    feat: add multi-key press to trigger Sol (392Hz) chord substitute

--- week2/digital_piano/digital_piano.ino
+++ week2/digital_piano/digital_piano.ino
@@ -1,9 +1,9 @@
 // Pin Definitions
 const int buzzerPin = 8;
-const int btn1 = 2; // Do
-const int btn2 = 3; // Re
-const int btn3 = 4; // Mi
-const int btn4 = 5; // Fa
+const int btn1 = 2; 
+const int btn2 = 3; 
+const int btn3 = 4; 
+const int btn4 = 5; 
 
 void setup() {
   pinMode(buzzerPin, OUTPUT);
@@ -14,10 +14,25 @@ void setup() {
 }
 
 void loop() {
-  // Check which button is pressed (LOW means pressed)
-  if (digitalRead(btn1) == LOW) tone(buzzerPin, 262);      // Do
-  else if (digitalRead(btn2) == LOW) tone(buzzerPin, 294); // Re
-  else if (digitalRead(btn3) == LOW) tone(buzzerPin, 330); // Mi
-  else if (digitalRead(btn4) == LOW) tone(buzzerPin, 349); // Fa
-  else noTone(buzzerPin);                                  // Silence if none pressed
+  // Read all button states
+  bool p1 = (digitalRead(btn1) == LOW);
+  bool p2 = (digitalRead(btn2) == LOW);
+  bool p3 = (digitalRead(btn3) == LOW);
+  bool p4 = (digitalRead(btn4) == LOW);
+
+  // Count how many buttons are actively pressed
+  int pressCount = p1 + p2 + p3 + p4;
+
+  if (pressCount >= 2) {
+    tone(buzzerPin, 392); // Play Sol for chord substitute
+  } 
+  else if (pressCount == 1) {
+    if (p1) tone(buzzerPin, 262);      // Do
+    else if (p2) tone(buzzerPin, 294); // Re
+    else if (p3) tone(buzzerPin, 330); // Mi
+    else if (p4) tone(buzzerPin, 349); // Fa
+  } 
+  else {
+    noTone(buzzerPin);
+  }
 }
