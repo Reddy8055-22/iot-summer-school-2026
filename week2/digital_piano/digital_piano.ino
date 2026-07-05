193cb10bf170f0cb47ade51b06d38c9789baf4d6
Reddy8055-22 <sushanthreddypalle22@gmail.com>
2026-07-05 15:21:20 +0000

    piano

--- week2/digital_piano/digital_piano.ino
+++ week2/digital_piano/digital_piano.ino
@@ -1,9 +1,14 @@
 // Pin Definitions
 const int buzzerPin = 8;
-const int btn1 = 2; 
-const int btn2 = 3; 
-const int btn3 = 4; 
-const int btn4 = 5; 
+const int btn1 = 2;
+const int btn2 = 3;
+const int btn3 = 4;
+const int btn4 = 5;
+const int modeBtn = 6; // 5th button for toggle
+
+// State Tracking
+bool isMinor = false;
+int lastModeState = HIGH;
 
 void setup() {
   pinMode(buzzerPin, OUTPUT);
@@ -11,25 +16,37 @@ void setup() {
   pinMode(btn2, INPUT_PULLUP);
   pinMode(btn3, INPUT_PULLUP);
   pinMode(btn4, INPUT_PULLUP);
+  pinMode(modeBtn, INPUT_PULLUP);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // Read all button states
+  // 1. Handle Mode Toggle Button (with debouncing)
+  int currentModeState = digitalRead(modeBtn);
+  if (lastModeState == HIGH && currentModeState == LOW) {
+    isMinor = !isMinor; // Flip the scale mode
+    Serial.println(isMinor ? "Scale: MINOR" : "Scale: MAJOR");
+    delay(50); // Basic debounce delay
+  }
+  lastModeState = currentModeState;
+
+  // 2. Read Piano Keys
   bool p1 = (digitalRead(btn1) == LOW);
   bool p2 = (digitalRead(btn2) == LOW);
   bool p3 = (digitalRead(btn3) == LOW);
   bool p4 = (digitalRead(btn4) == LOW);
 
-  // Count how many buttons are actively pressed
   int pressCount = p1 + p2 + p3 + p4;
 
+  // 3. Play Tones
   if (pressCount >= 2) {
-    tone(buzzerPin, 392); // Play Sol for chord substitute
+    tone(buzzerPin, 392); // Sol 
   } 
   else if (pressCount == 1) {
-    if (p1) tone(buzzerPin, 262);      // Do
+    if (p1) tone(buzzerPin, 262); // Do
     else if (p2) tone(buzzerPin, 294); // Re
-    else if (p3) tone(buzzerPin, 330); // Mi
+    // Switch Mi frequency based on scale mode
+    else if (p3) tone(buzzerPin, isMinor ? 311 : 330); // Minor(Eb) vs Major(E)
     else if (p4) tone(buzzerPin, 349); // Fa
   } 
   else {
